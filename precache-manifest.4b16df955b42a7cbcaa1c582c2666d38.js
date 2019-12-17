eedd96896047cde106d414c06fcb988878abd209
AkaZuko <f2013083@goa.bits-pilani.ac.in>
2019-12-17 21:15:22 +0000

    Deploying to master from master 4b10da60bc189d175f1cf34318aff498686b9040

---
+++ precache-manifest.4b16df955b42a7cbcaa1c582c2666d38.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98b5c387911db1219af4d02a62354eb2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe27753aa70cb23ebe62",
+    "url": "/static/css/2.bea6f0b1.chunk.css"
+  },
+  {
+    "revision": "7eee8dfcbd91cd512b8e",
+    "url": "/static/css/main.54c18f54.chunk.css"
+  },
+  {
+    "revision": "fe27753aa70cb23ebe62",
+    "url": "/static/js/2.c55c574e.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/static/js/2.c55c574e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7eee8dfcbd91cd512b8e",
+    "url": "/static/js/main.9381e492.chunk.js"
+  },
+  {
+    "revision": "058eec7f45d9072bee76",
+    "url": "/static/js/runtime-main.c1f4bcb5.js"
+  }
+]);
